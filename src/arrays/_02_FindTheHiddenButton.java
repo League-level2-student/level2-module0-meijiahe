de53ee69e1d25ee4aed4225ab3d9ed1f4ce5e263
league <league@imac-5.attlocal.net>
2019-09-01 15:59:59 -0700

    FindtheHiddenButton

--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -20,9 +20,9 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton []buttons;
 	//2 create an int variable called hiddenButton
-	
+	int hiddenButton;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
@@ -33,24 +33,34 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
+		String answer=JOptionPane.showInputDialog(null,"Enter a positive number");
+		int x =Integer.parseInt(answer);
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
+		buttons=new JButton [x];
 		//5. Make a for loop to iterate through the JButton array
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
 			//8. add each JButton to the panel
-		
+		for(int i=0;i<buttons.length;i++) {
+		buttons[i]=new JButton();
+		buttons[i].addActionListener(this);
+		panel.add(buttons[i]);
+		}
 		//9 add the panel to the window
-		
+		window.add(panel);
 		//10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-		
+		window.setExtendedState(window.MAXIMIZED_BOTH);
 		//11. set the JFrame to visible.
-		
+		window.setVisible(true);
 		//12. Give the user the instructions for the game.
-		
+		JOptionPane.showMessageDialog(null, "The intruction for the game is to find the hidden button. ");
 		//13. initialize the hiddenButton variable to a random number less than the int created int step 3
+		Random r=new Random();
+		//int hb=hiddeden
+		//hiddenButton <x
 		
+			
+
 		//14. Set the text of the JButton located at hiddenButton to  "ME"
 
 		//15. Use Thread.sleep(1000); to pause the program.
